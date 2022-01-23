f8baca90f8aa03fcc0c9358540d738c60b71498c
ryanfernando21 <ryanfernando12@gmail.com>
2022-01-23 17:45:37 +0100

    Initial Commit

---
+++ Chess Visualisation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chess Visualisation
+//
+//  Created by Ryan Fernando on 23/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
